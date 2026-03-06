87c3e42af5ec39cf6041dced1bf86209e4b81ac7
AgbogidiRhunor <roobill1234@gmail.com>
2026-03-07 00:59:16 +0100

    Updated Urls.py for deployment

--- health_plus/urls.py
+++ health_plus/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
+def healthz(request):
+    return HttpResponse("OK", status=200)
+
 urlpatterns = [
+    path('healthz', healthz),
     path('admin/', admin.site.urls),
     path('', include('management.urls')),
     path('pharmacy/', include('pharmacy.urls')),
